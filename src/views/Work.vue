45fe23f8ade24a5ad7dcd5c36e293733cfa3cf0e
amgmetallica <gareraanthony@gmail.com>
2017-06-27 21:24:17 -0400

    Adding Grid for Work

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,12 +1,151 @@
 <template>
+<div class="work">
+  <section class="hero hero-image is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Work
+          </h1>
+          <h2 class="subtitle">
 
+          </h2>
+        </div>
+      </div>
+    </section>
+<section>
+  <span class="has-text-centered subtitle brand-title-text">Brands We've Worked With</span>
+  <!--Grid of Clients-->
+  <div class="columns is-gapless">
+    <div class="column is-text-centered">
+        <figure class="imghvr-zoom-in">
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          <figcaption>
+            <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+            This is some text
+          </figcaption>
+        </figure>
 
+    </div>
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+  </div>
+
+  <div class="columns is-gapless">
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+  </div>
+
+  <div class="columns is-gapless">
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+  </div>
+
+  <div class="columns is-gapless">
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+  </div>
+
+  <div class="columns is-gapless">
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+    <div class="column is-text-centered">
+      <figure class="imghvr-zoom-in">
+        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+        <figcaption>
+          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
+          This is some text
+        </figcaption>
+      </figure>
+
+    </div>
+  </div>
+</section>
+</div>
 </template>
 <script>
   export default {
     name: 'Work'
   }
 </script>
-<style lang="scss">
-
+<style lang="scss" scoped>
+.hero-image {
+  background-image: url('https://unsplash.it/1920/1080');
+}
+  .title {
+    color: #fff;
+  }
+.brand-title-text {
+ color: #000;
+  padding-bottom: 20px;
+}
 </style>
