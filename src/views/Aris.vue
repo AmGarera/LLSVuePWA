0be1c608c6e292548fe6862485a709180a43b237
Anthony <Gareraanthony@gmail.com>
2017-07-02 16:17:36 -0400

    Adjusting margin on client page

--- src/views/Aris.vue
+++ src/views/Aris.vue
@@ -6,7 +6,7 @@
 </div>
 </section>
 <section>
-  <div class="container is-fluid">
+  <div style="margin: 0px 80px" class="container is-fluid">
     <p><a href="https://www.arise.com">Arise(TM) Virtual Solutions </a>is a customer service support company in Miramar, Florida. </p>
     <!--Client Images-->
     <img src="../../static/img/HP/Arise_1-06.png" alt="Client Image">
