fba4bcd2d8f3b657a6125586f6a356a32b76ae9a
Anthony <Gareraanthony@gmail.com>
2017-07-02 17:48:17 -0400

    Added Shop placeholder into router and placed SVG on page.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Hello from '@/views/Hello'
 import Work from '@/views/Work'
 import About from '@/views/About'
 import Blog from '@/views/Blog'
+import Shop from '@/views/Shop'
 import Aris from '@/views/Aris'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
@@ -32,6 +33,11 @@ export default new Router({
       path: '/Blog',
       name: 'Blog',
       component: Blog
+    },
+    {
+      path: '/Shop',
+      name: 'Shop',
+      component: Shop
     },
     {
       path: '/Aris',
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,3 +1,12 @@
-<template></template>
-<script></script>
+<template>
+  <img src="../../static/img/HP/Shop-05.svg" alt="Shop">
+
+
+
+</template>
+<script>
+  export default {
+    name: 'Shop'
+  }
+</script>
 <style></style>
