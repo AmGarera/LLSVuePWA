06ff4725c4b72b6504d08a30466e7abfcdcc2a90
amgmetallica <gareraanthony@gmail.com>
2017-07-01 23:06:27 -0400

    Scaffold out the client page template.

--- src/views/Aris.vue
+++ src/views/Aris.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="Aris">
-<section>
-  Aris Virtual Solutions
+<section class="hero is-medium">
+<div class="hero-body aris-hero">
+
+</div>
 </section>
 <section>
   <div class="container is-fluid">
@@ -9,6 +11,40 @@
     <!--Client Images-->
     <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
     <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <p>We worked with the Arise (TM)  team based in Charlotte, North Carolina to update and reformat their flyers,
+      all based off of their new brand guidelines. We  created a series  of 3 flyers,
+      reinventing the way the information is communicated  through user experience-based solutions. </p>
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <p>Several other concepts for the social media banners made for Arise(TM)  Virtual Solutions.
+      Clean and minimal in design, these social media banners helped set the stage for the Arise(TM) rebranding.
+    </p>
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <div class="columns">
+      <div class="column">
+        <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+      </div>
+      <div class="column">
+        <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+      </div>
+    </div>
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <p>We worked with the team to create a series of email campaign headers that mirror their social media banners.</p>
+    <div class="columns">
+      <div class="column">
+        <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+      </div>
+      <div class="column">
+        <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+      </div>
+      <div class="column">
+        <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+      </div>
+    </div>
+    <img src="../../static/img/HP/CommunityFocused.png" alt="Client Image">
+    <p>All work done for Arise(TM) Virtual Solutions was based off their newly created brand guide and brand standards.
+      All work and flyers shown are copyright Arise(TM) Virtual Solutions.</p>
   </div>
 </section>
   </div>
@@ -18,4 +54,12 @@
     name: 'Aris'
   }
 </script>
-<style></style>
+<style>
+  .aris-hero {
+    background: url("../../static/img/HP/arise-hero.jpg") no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+</style>
