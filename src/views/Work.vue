0ebfcaac5899fbdaf5afb84575c6b046173eec06
Anthony <Gareraanthony@gmail.com>
2017-07-03 00:36:14 -0400

    Added Julia Assets and cleaned up Work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -46,7 +46,8 @@
       </router-link>
     </div>
   </div>
-  <div class="columns">
+
+  <div style="padding: 0px 40px;" class="columns ">
     <div class="column">
       <router-link to="/Travel-Julias-Way">
         <div class="card">
@@ -60,98 +61,19 @@
         </div>
       </router-link>
     </div>
-    <div class="column is-text-centered">
-      <div class="card">
-      <figure class="imghvr-zoom-in">
+
+    <div class="column">
+      <router-link to="/Aris">
         <div class="card">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
-              Hero title
-            </h1>
-            <h2 class="subtitle">
-              Hero subtitle
-            </h2>
+          <div class="card-image">
+            <figure class="image is-3by2 wrapper">
+              <img class="image-hover-opacity" src="../../static/img/HP/IMG_6393.jpg" alt="Image">
+
+              <p class="text-box has-text-centered title">I'm Text</p>
+            </figure>
           </div>
         </div>
-
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-        </div>
-      </figure>
-      </div>
-
-    </div>
-  </div>
-
-  <div class="columns is-gapless">
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
-    </div>
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
-    </div>
-  </div>
-
-  <div class="columns is-gapless">
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
-    </div>
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
-    </div>
-  </div>
-
-  <div class="columns is-gapless">
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
-    </div>
-    <div class="column is-text-centered">
-      <figure class="imghvr-zoom-in">
-        <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-        <figcaption>
-          <img src="../../static/img/HP/GoLocalHonoredhere2017_graphic.jpg">
-          This is some text
-        </figcaption>
-      </figure>
-
+      </router-link>
     </div>
   </div>
 </section>
