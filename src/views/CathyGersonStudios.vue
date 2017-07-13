e6cb5f6b25b0f0fff0139f037a7156b77bf1b414
amgmetallica <gareraanthony@gmail.com>
2017-07-13 19:57:23 -0400

    Updated Cathy Page

--- src/views/CathyGersonStudios.vue
+++ src/views/CathyGersonStudios.vue
@@ -8,31 +8,32 @@
     </section>
     <section>
       <div style="margin: 0px 80px" class="container is-fluid">
-        <p style="padding: 60px 0px;" class="header-font"><b>Julia is an entrepreneur, blogger, and travel consultant living in North Carolina.</b>
+        <p style="padding: 60px 0px;" class="header-font"><b>Cathy Gerson is a talented local ceramic artist specializing in architectural and handcrafted tiles. </b>
         </p>
         <!--Client Images-->
         <!--<img src="../../static/img/HP/tyler1-06.png" alt="Client Image">-->
-        <img src="../../static/img/Julia/longlivesimple-312.jpg" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy2-06-2.png" alt="Client Image">
         <p class="content-padding has-text-left">
-          Julia’s hand-drawn logo conveys a sense of travel-meets-luxury, and the logo as a whole reflects her personality and stands for the personalized travel experience she creates for each client.</p>
-        <img src="../../static/img/Julia/longlivesimple-338.jpg" alt="Client Image">
-        <img src="../../static/img/Julia/longlivesimple-325.jpg" alt="Client Image">
+          She runs an innovative ceramic studio where textures, metals, and clay are combined to create stunning classic automotive, decorative, and versatile clay works.</p>
+        <img src="../../static/img/CathyGersonStudios/Cathy3-06.png" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy4-06-2.png" alt="Client Image">
         <p class="content-padding has-text-left">
-          We created these personalized business cards for Julia, featuring her logo mark, map, contact information, and custom icon set.
         </p>
-        <img src="../../static/img/Julia/longlivesimple-329.jpg" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy5-06.png" alt="Client Image">
         <p class="content-padding has-text-left">
-          With the help of developer Anthony Garera, we crafted a fully customized website experience for our client Travel Julia’s Way. Julia’s new website, www.traveljuliasway.com, features Julia’s Destinations, Stories from the Road, Testimonials, Upcoming Trips and more.
+          Cathy’s logo was inspired by her classic car installations. We were honored to help her craft her new brand using her original signature she inscribes into her clay works.
         </p>
-        <img src="../../static/img/Julia/longlivesimple-423.jpg" alt="Client Image">
-        <img src="../../static/img/Julia/longlivesimple-456.jpg" alt="Client Image">
-        <img src="../../static/img/Julia/longlivesimple-451.jpg" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy6-06.png" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy7-06.png" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy8-06.png" alt="Client Image">
         <p class="content-padding has-text-left">
-          We used user experience practices to craft Julia an interactive site that showcases her main navigation using a custom created icon set. If you’d like to learn more about Julia and her story, visit her About page here.
+          As these clay totems show, Cathy adds mica and natural stones from the mountain stream behind her home studio to many of her glazed sculptures.
         </p>
-        <img src="../../static/img/Julia/IconDesign.png" alt="Client Image">
-        <img src="../../static/img/Julia/longlivesimple-438.jpg" alt="Client Image">
-        <img src="../../static/img/Julia/longlivesimple-437.jpg" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy9-06.png" alt="Client Image">
+        <img src="../../static/img/CathyGersonStudios/Cathy10-06.png" alt="Client Image">
+        <p class="content-padding has-text-left">
+          Closeup of Cathy’s classic motorcycle creations from clay.
+        </p>
       </div>
     </section>
   </div>
@@ -44,7 +45,7 @@
 </script>
 <style scoped>
   .aris-hero {
-    background: url("../../static/img/Julia/longlivesimple-437.jpg");
+    background: url("../../static/img/CathyGersonStudios/Cathy5-06-2.png");
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
